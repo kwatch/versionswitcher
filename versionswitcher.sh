f0973a5faec10b7d92441672792b953604edd440
makoto kuwata <kwa@kuwata-lab.com>
2011-05-01 10:48:28 +0900

    update help message of 'versionswitcher.sh'

--- versionswitcher.sh
+++ versionswitcher.sh
@@ -25,13 +25,16 @@ versionswitcher - change version of language or application
 release: $release
 examples:
     $ VERSIONSWITCHER_PATH=\$HOME/lang
-    $ vs -h             # show help
-    $ vs python 2.6.6   # use \$HOME/lang/python/2.6.6
-    $ vs python 2       # use \$HOME/lang/python/2.x.x (ex. 2.7.1)
-    $ vs python latest  # use latest version under \$HOME/lang/python
-    $ vs python -       # use system-installed one (ex. /usr/bin/python)
-    $ vs python         # show installed versions of python
-    $ vs                # show installed languages
+    $ vs -h               # show help
+    $ vs                  # list language names installed
+    $ vs python           # list python vesrions installed
+    $ vs python 2.6.6     # use \$HOME/lang/python/2.6.6
+    $ vs python 2         # use \$HOME/lang/python/2.x.x (ex. 2.7.1)
+    $ vs python latest    # use latest version under \$HOME/lang/python
+    $ vs python -         # use system-installed one (ex. /usr/bin/python)
+    $ vs -i               # list language names installable
+    $ vs -i python        # list python versions installable
+    $ vs -i python latest # install python latest version (ex. 3.2.0)
 
 tips:
     * Short name 'vs' is an alias to 'versionswitcher'.
