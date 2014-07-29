d2020884abe952a73f05bb16692cb4368eb58785
makoto kuwata <kwa@kuwata-lab.com>
2014-07-30 05:36:24 +0900

    enhance 'server' task recipe in 'website/Kookbook.py' to support port 80

--- website/Kookbook.py
+++ website/Kookbook.py
@@ -8,7 +8,8 @@ python   = prop('python', 'python')
 def server(c, *args, **kwargs):
     """start dev_appserver.py"""
     port = kwargs.get('p', 8080)
-    system(c%"$(python) $(gae_home)/dev_appserver.py --port=$(port) .")
+    sudo = int(port) < 1024 and 'sudo ' or ''
+    system(c%"$(sudo)$(python) $(gae_home)/dev_appserver.py --port=$(port) .")
 
 @recipe
 @spices("--cron: upload only 'cron.yaml'")
