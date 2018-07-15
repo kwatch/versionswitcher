e95b6b4c92e2cf10f10b5019f6e273e300898f69
kwatch <kwatch@gmail.com>
2018-07-15 10:11:32 +0900

    update 'pypy3' installer

--- installers/vs_install_pypy3.sh
+++ installers/vs_install_pypy3.sh
@@ -14,51 +14,81 @@ _install_pypy3() {
     local prefix=$2
     local lang="pypy3"
     local prompt="**"
-    ## platform-specific settings
-    case `uname` in
-    Darwin*|darwin*)
-        target="osx64";;
-    Linux*|linux*)
-        case `uname -m` in
-        x86_64|amd64)  target="linux64";;
-        i686|i386)     target="linux"  ;;
-        *)             target="linux"  ;;
-        esac
-        ;;
-    *)
-        target="src";;
-    esac
+    ## donwload, extract, and install
+    local perl
+    if   [ -f /usr/local/bin/perl ]; then perl="/usr/local/bin/perl"
+    elif [ -f /usr/bin/perl ];       then perl="/usr/bin/perl"
+    else                                  perl="perl"
+    fi
+    local url="https://bitbucket.org/pypy/pypy/downloads/"
+    local rexp='href="/pypy/pypy/downloads/(pypy3[.\d]*-v?([.\d]+)-\w+[-\w]*)\.tar\.bz2"';
+    down=`_downloader "-sL" "-q -O -"` || return 1
+    local items=`eval $down $url | $perl -e '
+        my $ver="'$version'";
+        while (<>) {
+            while (m!'$rexp'!g) {
+                print $1, "\n" if $2 eq $ver;
+            }
+        }
+    '`
+    echo "$prompt Which one to install?"
+    echo "$prompt"
+    local i=0;
+    for x in $items; do
+        i=`expr $i + 1`
+        echo "**    $i: $x";
+    done
+    echo "$prompt"
+    echo -n "$prompt Select [1-$i]: "
+    read input
+    local base
+    local j=0
+    for x in $items; do
+        j=`expr $j + 1`
+        if [ $j = "$input" ]; then
+            base=$x
+            break
+        fi
+    done
+    if [ -z "$base" ]; then
+        echo "$prompt ERROR: invalid selection." 1>&2
+        return 1
+    fi
     ## inform required libraries
-    if [ "$target" = "src" ]; then
+    local src_p=''
+    case $base in
+        *-src)  src_p="Y";;
+    esac
+    if [ -n "$src_p" ]; then
         _vs_inform_required_libraries "$prompt"   || return 1
     fi
-    ## donwload, extract, and install
-    local ver=`echo $version | sed 's/\.0$//'`
-    local base="pypy3-$ver-$target"
-    local url="https://bitbucket.org/pypy/pypy/downloads/$base.tar.bz2"
+    #
+    local dlurl="https://bitbucket.org/pypy/pypy/downloads/$base.tar.bz2"
     if [ ! -e "$base.tar.bz2" ]; then
         local down
-        down=`_downloader "-LRO" "--no-check-certificate"` || return 1
-        _cmd "$down $url"                         || return 1
+        #down=`_downloader "-LRO" "--no-check-certificate"` || return 1
+        down=`_downloader "-LRO" ""`              || return 1
+        _cmd "$down $dlurl"                       || return 1
     fi
     _cmd "rm -rf $base"                           || return 1
     _cmd "tar xjf $base.tar.bz2"                  || return 1
-    local dirname=`tar tjf $base.tar.bz2 | grep ^pypy | awk -F/ 'NR==1 { print $1 }'`
+    local dirname=`tar tjf $base.tar.bz2 | awk -F/ 'NR==1 { print $1 }'`
     if [ -z "$dirname" ]; then
         echo "$prompt ERROR: failed to detect archive directory name." 1>&2
         echo "$prompt exit 1" 1>&2
         return 1
     fi
+    #
     local opt=""
     local basedir=""
-    if [ "$target" = "src" ]; then
+    if [ -n "$src_p" ]; then
         opt="-Ojit"                # get the JIT version
         #opt="-O2"                  # get the no-jit version
         #opt="-O2 --sandbox"        # get the sandbox version
         #opt="-O2 --stackless"      # get the stackless version
         #opt="-Ojit --backend=cli"  # only for branch/cli-jit
         _cmd "cd $dirname/pypy/translator/goal"   || return 1
-        _cmd "time pypy translate.py $opt"        || return 1
+        _cmd "time pypy3 translate.py $opt"       || return 1
         _cmd "cd ../../../.."                     || return 1
     else
         basedir=`dirname $prefix`
@@ -66,48 +96,46 @@ _install_pypy3() {
         _cmd "mkdir -p $basedir"                  || return 1
         _cmd "mv $dirname $prefix"                || return 1
     fi
-    ## create a link of 'bin/pypy' as 'bin/python'
+    ## create a link of 'bin/pypy3' as 'bin/python3'
     local input=""
-    echo -n "$prompt Create a link of 'bin/pypy' as 'bin/python'? [Y/n]: "
+    echo -n "$prompt Create a link of 'bin/pypy3' as 'bin/python3'? [Y/n]: "
     read input;  [ -z "$input" ] && input="y"
     case "$input" in
-    y*|Y*)  _cmd "(cd $prefix/bin/; ln -s pypy python)" ;;
+    y*|Y*)  _cmd "(cd $prefix/bin/; ln -s pypy3 python; n -s pypy3 python3)" ;;
     esac
     ## verify
     _cmd "export PATH=$prefix/bin:$PATH"          || return 1
     _cmd "hash -r"                                || return 1
-    _cmd "which pypy"                             || return 1
-    if [ "$prefix/bin/pypy" != `which pypy` ]; then
-        echo "$prefix ERROR: pypy command seems not installed correctly." 1>&2
+    _cmd "which pypy3"                            || return 1
+    if [ "$prefix/bin/pypy3" != `which pypy3` ]; then
+        echo "$prefix ERROR: pypy3 command seems not installed correctly." 1>&2
         echo "$prefix exit 1" 1>&2
         return 1
     fi
-    ## install 'easy_install'
-    local easy_install_path
-    local script='distribute_setup.py'
-    echo
-    echo -n "$prompt Install 'easy_install' command? [Y/n]: "
-    read input;  [ -z "$input" ] && input="y"
-    case "$input" in
-    y*|Y*)
-        url="http://python-distribute.org/$script"
-        local down
-        down=`_downloader "-ORL" "-N"`            || return 1
-        _cmd "$down $url"                         || return 1
-        _cmd "$prefix/bin/pypy $script"           || return 1
-        _cmd "which easy_install"                 || return 1
-        easy_install_path=`which easy_install`
-        if [ "$easy_install_path" != "$prefix/bin/easy_install" ]; then
-            echo "$prompt ERROR: easy_install command seems not installed correctly." 1>&2
-            echo "$prompt exit 1" 1>&2
-            return 1
-        fi
-        echo "$prompt easy_install command installed successfully."
-        ;;
-    *)
-        echo "$prompt skip to install easy_install command."
-        ;;
-    esac
+    ## install 'pip'
+    if [ ! -e "$prefix/bin/pip" ]; then
+        echo
+        echo -n "$prompt Install 'pip' command? [Y/n]: "
+        read input;  [ -z "$input" ] && input="y"
+        case "$input" in
+        y*|Y*)
+            local dlurl="https://bootstrap.pypa.io/get-pip.py"
+            local down
+            down=`_downloader "-ORL" "-N"`            || return 1
+            _cmd "$down $dlurl"                       || return 1
+            _cmd "$prefix/bin/pypy3 get-pip.py"        || return 1
+            if [ ! -e "$prefix/bin/pip" ]; then
+                echo "$prompt ERROR: failed to install 'pip' command." 1>&2
+                echo "$prompt exit 1" 1>&2
+                return 1
+            fi
+            echo "$prompt 'pip' command installed successfully."
+            ;;
+        *)
+            echo "$prompt skip to install 'pip' command."
+            ;;
+        esac
+    fi
     ## finish
     echo
     echo "$prompt Installation finished successfully."
