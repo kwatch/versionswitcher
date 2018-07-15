167cfe4c4188d52a1857f35dc796c98a22c0e4df
kwatch <kwatch@gmail.com>
2018-07-15 14:45:06 +0900

    change 'node' installer to skip 'npm' installation when 'npm' already exists

--- installers/vs_install_node.sh
+++ installers/vs_install_node.sh
@@ -62,29 +62,31 @@ _install_node() {
         return 1
     fi
     ## install 'npm' (Node Package Manager)
-    echo
-    echo -n "$prompt Install npm (Node Package Manger)? [y/N]: "
-    read input;  [ -z "$input" ] && input="n"
-    case "$input" in
-    y*|Y*)
-        #_cmd "curl https://npmjs.org/install.sh | sh" || return 1
-        #_cmd "wget -qO - https://npmjs.org/install.sh | sh" || return 1
-        local down
-        down=`_downloader "-LRO" "-N"`            || return 1
-        _cmd "$down https://npmjs.org/install.sh" || return 1
-        _cmd "sh install.sh"                      || return 1
-        local npm_path=`which npm`
-        if [ "$npm_path" != "$prefix/bin/npm" ]; then
-            echo "$prompt ERROR: npm command seems not installed correctly." 1>&2
-            echo "$prompt exit 1" 1>&2
-            return 1
-        fi
-        echo "$prompt npm installed successfully."
-        ;;
-    *)
-        echo "$prompt skip to install npm."
-        ;;
-    esac
+    if [ ! -e "$prefix/bin/npm" ]; then
+        echo
+        echo -n "$prompt Install npm (Node Package Manger)? [y/N]: "
+        read input;  [ -z "$input" ] && input="n"
+        case "$input" in
+        y*|Y*)
+            #_cmd "curl https://npmjs.org/install.sh | sh" || return 1
+            #_cmd "wget -qO - https://npmjs.org/install.sh | sh" || return 1
+            local down
+            down=`_downloader "-LRO" "-N"`            || return 1
+            _cmd "$down https://npmjs.org/install.sh" || return 1
+            _cmd "sh install.sh"                      || return 1
+            local npm_path=`which npm`
+            if [ "$npm_path" != "$prefix/bin/npm" ]; then
+                echo "$prompt ERROR: npm command seems not installed correctly." 1>&2
+                echo "$prompt exit 1" 1>&2
+                return 1
+            fi
+            echo "$prompt npm installed successfully."
+            ;;
+        *)
+            echo "$prompt skip to install npm."
+            ;;
+        esac
+    fi
     ## finish
     echo
     echo "$prompt Installation finished successfully."
