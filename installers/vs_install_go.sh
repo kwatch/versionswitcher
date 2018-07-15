658790d0cf93adf887e46702d9f45a5cd06d0b8b
kwatch <kwatch@gmail.com>
2018-07-15 18:36:13 +0900

    fix 'go' installer to retrieve version correctly

--- installers/vs_install_go.sh
+++ installers/vs_install_go.sh
@@ -29,7 +29,7 @@ _install_go() {
     $down $archives_url | $perl -e '
     my $i = 0;
     while (<>) {
-      if (/href="https:\/\/dl\.google\.com\/go\/(go'$verpat'\..*?\.tar\.gz)">go/) {
+      if (/href="https:\/\/dl\.google\.com\/go\/(go'$verpat'\.[^.]+\.tar\.gz)">go/) {
         print "\n** Which one to install?\n" if $i == 0;
         print ++$i, ": ", $1, "\n";
       }
